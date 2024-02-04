8c3508da763fab791bffa3f3b35486746b61ebe9
Joshua Goldstein <joshua-github@pm.me>
2024-02-04 17:07:26 -0600

    grr

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://joshua-goldstein.github.io/joshua-goldstein"
+# not needed if using action
+# SITEURL = "https://joshua-goldstein.github.io/joshua-goldstein"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
